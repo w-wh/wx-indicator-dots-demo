c3fa57d9cba4a1eeab3e1258178c88d3416beb64
王文豪 <961383696@qq.com>
2019-10-14 16:10:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# wx-indicator-dots-demo
---
+++ pages/home/home.js
@@ -0,0 +1,72 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      '../../static/images/banner1.jpg',
+      '../../static/images/banner2.jpg',
+      '../../static/images/banner3.jpg',
+      '../../static/images/banner4.jpg',
+      '../../static/images/banner5.jpg'
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
